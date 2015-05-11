5bfd6e97a633a6bc0c86613c1e8e7c8c5ae24b46
Martin Galese <martin@galese.net>
2015-05-10 20:03:07 -0400

    Blending Ops

--- src/main/scala-2.11/Booleans.scala
+++ src/main/scala-2.11/Booleans.scala
@@ -22,3 +22,25 @@ case class Intersection(val a: FRep, val b: FRep) extends FRep {
     fa + fb - math.sqrt(fa * fa + fb * fb)
   }
 }
+
+case class BlendUnion(val f1: FRep, val f2: FRep, val a0: Double, val a1: Double, val a2: Double) extends FRep {
+  def f(c: Coordinate) = {
+    val f1c = f1.f(c)
+    val f2c = f2.f(c)
+    val f1a1 = math.pow(f1c/a1,2)
+    val f2a2 = math.pow(f2c/a2,2)
+
+    f1c + f2c + math.sqrt(f1c*f1c+f2c*f2c) + a0 / (1+f1a1+f2a2)
+  }
+}
+
+case class BlendIntersect(val f1: FRep, val f2: FRep, val a0: Double, val a1: Double, val a2: Double) extends FRep {
+  def f(c: Coordinate) = {
+    val f1c = f1.f(c)
+    val f2c = f2.f(c)
+    val f1a1 = math.pow(f1c/a1,2)
+    val f2a2 = math.pow(f2c/a2,2)
+
+    f1c + f2c - math.sqrt(f1c*f1c+f2c*f2c) + a0 / (1+f1a1+f2a2)
+  }
+}
--- src/main/scala-2.11/main.scala
+++ src/main/scala-2.11/main.scala
@@ -10,12 +10,11 @@ import Transform._
 object main extends App {
   val space = new Space()
 
-  val scene = TorusX(15,30)
+  //val scene = TorusX(15,30)
   //val scene = CylinderX(10,30)
   //val scene = RotateZ(Box(-10,10,-10,10,-10,10), math.Pi/4)
-  //val scene = Union(Sphere(20), Translate(Sphere(20),Coordinate(10,10,0)))
+  val scene = BlendUnion(Sphere(20), Translate(Sphere(10),Coordinate(20,20,0)),5,1,1)
   //val scene = Intersection(Sphere(20), Translate(Sphere(20), Coordinate(10,10,0)))
   //val scene = Difference(Sphere(20), Translate(Sphere(20), Coordinate(10,10,0)))
-  //val scene = Sphere(8)
   space.to_pointcloud(scene, "out.xyz")
 }
